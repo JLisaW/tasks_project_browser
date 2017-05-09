4cdbf9f98c64eab7772b0b8d9cce581a0796d36a
JLisaW <j.lisa.wang@gmail.com>
2017-05-09 09:46:50 -0400

    change config value for production
    add s to http

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'http://calm-basin-94254.herokuapp.com/',
+    production: 'https://calm-basin-94254.herokuapp.com/',
     development: 'http://localhost:4741'
   }
 }
